cc7e96d580898e71ff34c2b264838e1ba4658b45
Paras Ahuja <parasxahuja@gmail.com>
2024-04-27 18:30:17 +0530

    update vite.config.js

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_SERVER_URL,
+        target: `${process.env.VITE_SERVER_URL}`,
         changeOrigin: true,
       },
     },
