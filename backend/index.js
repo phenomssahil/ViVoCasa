3e411ba49c20a559ef3a80ffdfdce2dd8eb807b5
Paras Ahuja <parasxahuja@gmail.com>
2024-04-27 23:20:35 +0530

    update cors credentials:true

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,8 @@ const cors = require('cors');
 app.use(cors({
     origin: `${process.env.CLIENT_URL}`,
     methods:['GET','POST','PUT','DELETE'],
-    allowedHeaders:['Content-Type']
+    allowedHeaders:['Content-Type'],
+    credentials:true
 }));
 
 const {checkAuth} = require('./middleware/authMiddleware')
